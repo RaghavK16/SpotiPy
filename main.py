94bce800356b4dbf81dd5d3b70c922d29a3790bf
Raghav Khullar <raghav.khullar@zs.com>
2021-06-11 03:31:54 +0530

    Adding main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import sqlalchemy
+import pandas as pd
+from sqlalchemy.orm import sessionmaker
+import requests
+import json
+import datetime
+from datetime import datetime
+import sqlite3
+
+DATABASE_LOCATION = ""

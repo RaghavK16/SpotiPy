6a83128c7244d9f0c5b5489e1c9b40ad5c6462b0
Raghav Khullar <raghav.khullar@zs.com>
2021-08-29 02:42:51 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ from datetime import datetime
 import sqlite3
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER_ID = "RaghavK"
-TOKEN = "BQAw-poCgFWWgMWgbQT5Fl_5MHQyzHtdZRlgVxhbrNBtvVg5eE_Q5dm5d1qS2X1Yrq12yW-8ZhsvzMO0WuG0yKJh7teY0gw10e7M4D1JyQbLQ3tq_kVfrm_9Me4kGcwzuGTZd6Fja8rv5MYO6zvpjGbhFLvp1V8WCJX482DF"
+USER_ID = "RaghavK16"
+TOKEN = "BQC9U1LvuqO34zyKPOZtHq0hoc7FmsdspfWwwJgUQlNFs72RQ4N63PMHdcl1qqx5hLAPusqxwl62bUaz4fFAGCGMQO1UoKCpXMi9Z0J-9wMPh8vHVUmyiz2Xakmk6lm8MHY4S5KVowZ1tFVug89mjH2QuAN14P1xcFoUkbf9"
 
 if __name__ == "__main__":
 
     headers = {
         "Accept": "application/json",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        Authorization: "Bearer {token}".format(token=TOKEN)
     }
+
+    today = datetime.datetime.now()
+    yesterday = today - datetime.timedelta(days=1)
+    yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+
+    r = requests.get("https://developer.spotify.com/console/get-recently-played/?after=(time)".format(time=yesterday_unix_timestamp), headers=headers)
+
+    data = r.json()
+
+    print(data)
